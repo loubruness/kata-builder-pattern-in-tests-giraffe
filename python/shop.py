0b2ca48cc98ba9d9b39dcba157c77488b8d88325
{Arthur Billebaut} <{arthur.billebaut@efrei.net}>
2024-11-27 11:03:27 +0100

    error found + builder class created + test passed

--- python/shop.py
+++ python/shop.py
@@ -17,6 +17,37 @@ class User:
     age: int
     address: Address
     verified: bool
+    
+    class Builder:
+        def __init__(self):
+            self.name = None
+            self.email = None
+            self.age = None
+            self.address = None
+            self.verified = None
+
+        def with_name(self, name):
+            self.name = name
+            return self
+
+        def with_email(self, email):
+            self.email = email
+            return self
+
+        def with_age(self, age):
+            self.age = age
+            return self
+
+        def with_address(self, address):
+            self.address = address
+            return self
+
+        def with_verified(self, verified):
+            self.verified = verified
+            return self
+
+        def build(self):
+            return User(self.name, self.email, self.age, self.address, self.verified)
 
 
 class Shop:
@@ -25,10 +56,12 @@ class Shop:
         if user.age <= 18:
             return False
         if not user.verified:
-            return True
+            return False #FIXME: c'était kc
         else:
             return True
 
     @classmethod
     def must_pay_foreign_fee(cls, user):
         return user.address.country != "USA"
+
+        
--- python/tests/test_shop.py
+++ python/tests/test_shop.py
@@ -2,49 +2,53 @@ from shop import Shop, User
 
 
 def test_happy_path(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=fsf_address,
-        verified=True,
-    )
+    user = User.Builder().with_age(25).with_verified(True).with_address(fsf_address).build()
+    # user = User(
+    #     name="bob",
+    #     email="bob@domain.tld",
+    #     age=25,
+    #     address=fsf_address,
+    #     verified=True,
+    # )
 
     assert Shop.can_order(user)
     assert not Shop.must_pay_foreign_fee(user)
 
 
-def test_minors_cannot_order_from_the_shop(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=16,
-        address=fsf_address,
-        verified=True,
-    )
+def test_minors_cannot_order_from_the_shop():
+    user = User.Builder().with_age(17).build()
+    # user = User(
+    #     name="bob",
+    #     email="bob@domain.tld",
+    #     age=16,
+    #     address=fsf_address,
+    #     verified=True,
+    # )
 
     assert not Shop.can_order(user)
 
 
-def test_cannot_order_if_not_verified(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=16,
-        address=fsf_address,
-        verified=False,
-    )
+def test_cannot_order_if_not_verified():
+    user = User.Builder().with_age(19).with_verified(False).build()
+    # user = User(
+    #     name="bob",
+    #     email="bob@domain.tld",
+    #     age=16, #FIXME: faut pas tester pour mineur faut tester que pour not verified
+    #     address=fsf_address,
+    #     verified=False,
+    # )
 
     assert not Shop.can_order(user)
 
 
 def test_foreigners_must_be_foreign_fee(paris_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=paris_address,
-        verified=False,
-    )
+    user = User.Builder().with_address(paris_address).build()
+    # user = User(
+    #     name="bob",
+    #     email="bob@domain.tld",
+    #     age=25,
+    #     address=paris_address,
+    #     verified=False,
+    # )
 
     assert Shop.must_pay_foreign_fee(user)
